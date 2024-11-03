302abfd35ef7f9e54e507466708cde0e9cd12bfa
Sudipta Mondal <msudipta857@gmail.com>
2024-11-03 12:54:49 +0530

    fix error

--- db/database.js
+++ db/database.js
@@ -1,5 +1,7 @@
 const mongoose = require ('mongoose');
-
+require('dotenv').config();
+const express = require('express');
+const app = express();
 module.exports= ()=>{
   return  mongoose.connect(process.env.MONGODB_API,{
     useNewUrlParser: true,
@@ -13,5 +15,9 @@ module.exports= ()=>{
       console.error('MongoDB connection error:', error);
 
     })
-
 }
+mongoose.connection.once('open', () => {
+  app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+  });
+});
